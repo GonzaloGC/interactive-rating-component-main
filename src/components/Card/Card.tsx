b7413a0dba1f1fa7e1049a60efa0dd8df11a71fc
Gonzalo <elias.gonzaloguerrero@gmail.com>
2024-05-15 03:30:45 -0400

    added mobile styles and finished

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -6,20 +6,22 @@ import { ButtonSubmit } from "../ButtonSubmit/ButtonSubmit";
 export const Card = () => {
   return (
     <section className="container-card">
-      <CircleStar />
-      <h1 className="title-card">How did we do?</h1>
-      <p className="p-card">
-        Please let us Know how we did with your support request. All feedback is
-        appreciated to help us improve our offerinq!
-      </p>
-      <div className="container-circle-number">
-        <CircleNumber number="1" />
-        <CircleNumber number="2" />
-        <CircleNumber number="3" />
-        <CircleNumber number="4" />
-        <CircleNumber number="5" />
+      <div className="divTest">
+        <CircleStar />
+        <div className="title-card">How did we do?</div>
+        <div className="p-card">
+          Please let us Know how we did with your support request. All feedback
+          is appreciated to help us improve our offerinq!
+        </div>
+        <div className="container-circle-number">
+          <CircleNumber number="1" />
+          <CircleNumber number="2" />
+          <CircleNumber number="3" />
+          <CircleNumber number="4" />
+          <CircleNumber number="5" />
+        </div>
+        <ButtonSubmit nameButton="submit" />
       </div>
-      <ButtonSubmit nameButton="submit"/>
     </section>
   );
 };
