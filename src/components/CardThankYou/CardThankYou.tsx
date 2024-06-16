88c5af16ecb6ea539c19d43107270f047fe41a30
Gonzalo <elias.gonzaloguerrero@gmail.com>
2024-06-15 20:02:40 -0400

    added return icon

--- src/components/CardThankYou/CardThankYou.tsx
+++ src/components/CardThankYou/CardThankYou.tsx
@@ -1,24 +1,16 @@
 import "./CardThankYou.css";
 import { motion } from "framer-motion";
-// import { RxUpdate } from "react-icons/rx";
-import { GoSync } from "react-icons/go";
-// import { ReloadIcon } from "../icon/Reload";
+import { LinkBackwardIcon } from "../icon/LinkBackwardIcon";
 import { useState } from "react";
 import { Card } from "../Card/Card";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
-// type cardThankYouProps = {
-//   selectMessage: number;
-// };
 interface LocationState {
   renderedNumber: number | null;
 }
 
 export function CardThankYou() {
-  // export function CardThankYou(props: cardThankYouProps) {
-  // const { selectMessage } = props;
-
   const location = useLocation();
   const state = location.state as LocationState;
   const renderedNumber = state?.renderedNumber ?? null;
@@ -37,15 +29,16 @@ export function CardThankYou() {
     >
       <section className="container-card-thank-you container-card-thank-you-out">
         <div className="container-info-card">
-          <Link onClick={handleClick} to="/home" className="animate-update">
-            {/* <motion.div
-                style={{ display: "inline-block" }}
-                initial={{ rotate: 0 }}
-                animate={{ rotate: -360 }}
-                transition={{ duration: 6, repeat: Infinity, ease: "linear" }}
-                > */}
-            <GoSync style={{ transformOrigin: "center" }} />
-            {/* </motion.div> */}
+          <Link
+            onClick={handleClick}
+            to="/home"
+            className="container-animate-update"
+            title="Return"
+          >
+            <div className="animate-update">
+              <LinkBackwardIcon />
+              {/* <GoSync style={{ transformOrigin: "center" }} /> */}
+            </div>
           </Link>
           <div className="container-images">
             <img
